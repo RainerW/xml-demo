72160aeb8952969bbee02e84884ab830881d22b8
baranowski <baranowski@1b5c7aab-be0c-0410-8b4d-f8b9e84c49a3>
2012-03-16 15:53:38 +0000

    add dom read example.
    git-svn-id: http://svn1.seitenbau.net/entwicklung/java/seitenbau/demo/xml-demo/trunk@89988 1b5c7aab-be0c-0410-8b4d-f8b9e84c49a3

--- src/test/java/com/seitenbau/demo/DomBookExampleTest.java
+++ src/test/java/com/seitenbau/demo/DomBookExampleTest.java
@@ -3,68 +3,29 @@ package com.seitenbau.demo;
 import static org.junit.Assert.*;
 
 import java.io.InputStream;
-import java.util.Collection;
-import java.util.Vector;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
 
 public class DomBookExampleTest
 {
 
+  private static final String CONTENT_NODE = "content";
+  private static final String TITLE_NODE = "title";
   private static final String NUMBER_ATTR = "number";
   private static final String CHAPTER_NODE = "chapter";
 
-  class Book
-  {
-
-    Collection<Chapter> chapters = new Vector<DomBookExampleTest.Chapter>();
-    
-    @Override
-    public int hashCode()
-    {
-      return HashCodeBuilder.reflectionHashCode(this);
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-      return EqualsBuilder.reflectionEquals(this, obj);
-    }
-  }
-
-  class Chapter
-  {
-
-    int number;
-    
-    String title;
-
-    String content;
-    
-    @Override
-    public int hashCode()
-    {
-      return HashCodeBuilder.reflectionHashCode(this);
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-      return EqualsBuilder.reflectionEquals(this, obj);
-    }
-  }
-  
   InputStream bookInputStream;
 
   @Before
@@ -82,14 +43,61 @@ public class DomBookExampleTest
   @Test
   public void testDomReadXml() throws Exception
   {
-    Book book = new Book();
-    
     // create DOM Document
     DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
     DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
     Document document = documentBuilder.parse(bookInputStream);
 
     // Parse the XML File
+    Book book = parse(document);
+    
+    // Verify the result
+    assertEquals(TestData.expectedBook, book);
+  }
+  
+  @Test
+  public void testDomWriteXml() throws Exception
+  {
+    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+    
+    // create a new book document
+    Document document = documentBuilder.newDocument();
+    Element bookNode = document.createElement("book");
+    document.appendChild(bookNode);
+    {
+      Element chapterNode = document.createElement(CHAPTER_NODE);
+      chapterNode.setAttribute("number", "1");
+      Element titleNode = document.createElement(TITLE_NODE);
+      titleNode.setTextContent("DOM");
+      chapterNode.appendChild(titleNode);
+      Element contentNode = document.createElement(CONTENT_NODE);
+      contentNode.setTextContent("Reads the whole XML document into the memory...");
+      chapterNode.appendChild(contentNode);
+      bookNode.appendChild(chapterNode);
+    }
+    {
+      Element chapterNode = document.createElement(CHAPTER_NODE);
+      chapterNode.setAttribute("number", "2");
+      Element titleNode = document.createElement(TITLE_NODE);
+      titleNode.setTextContent("SAX");
+      chapterNode.appendChild(titleNode);
+      Element contentNode = document.createElement(CONTENT_NODE);
+      contentNode.setTextContent("Reads NOT the whole XML document into the memory...");
+      chapterNode.appendChild(contentNode);
+      bookNode.appendChild(chapterNode);
+    }
+    
+    // parse the dom
+    Book book = parse(document);
+    
+    // Verify the result
+    assertEquals(TestData.expectedBook, book);
+  }
+
+  Book parse(Document document)
+  {
+    Book book = new Book();
     Node bookNode = document.getFirstChild();
     for(Node chapterNode = bookNode.getFirstChild(); chapterNode != null;){
       String nodeName = chapterNode.getNodeName();
@@ -107,12 +115,12 @@ public class DomBookExampleTest
         for(Node chapterChildNode = chapterNode.getFirstChild(); chapterChildNode != null;)
         {
           String chapterChildNodeName = chapterChildNode.getNodeName();
-          if(StringUtils.equals(chapterChildNodeName, "title"))
+          if(StringUtils.equals(chapterChildNodeName, TITLE_NODE))
           {
             String textContent = chapterChildNode.getTextContent();
             chapter.title = StringUtils.trim(textContent);
           }
-          else if(StringUtils.equals(chapterChildNodeName, "content"))
+          else if(StringUtils.equals(chapterChildNodeName, CONTENT_NODE))
           {
             String textContent = chapterChildNode.getTextContent();
             chapter.content = StringUtils.trim(textContent);
@@ -122,20 +130,6 @@ public class DomBookExampleTest
       }
       chapterNode = chapterNode.getNextSibling();
     }
-    
-    // Verify the result
-    Book expectedBook = new Book();
-    Chapter chapter1 = new Chapter();
-    chapter1.number = 1;
-    chapter1.title = "DOM";
-    chapter1.content = "Reads the whole XML document into the memory...";
-    expectedBook.chapters.add(chapter1);
-    Chapter chapter2 = new Chapter();
-    chapter2.number = 2;
-    chapter2.title = "SAX";
-    chapter2.content = "Reads NOT the whole XML document into the memory...";
-    expectedBook.chapters.add(chapter2);
-    
-    assertEquals(expectedBook, book);
+    return book;
   }
 }
